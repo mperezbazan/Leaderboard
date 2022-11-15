a98153fb2b132143aa3629d0cced840544174d2b
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-15 10:53:45 -0300

    Add style.css and list data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-function component() {
-  const element = document.createElement('div');
+import './style.css'
+const data = [
+  {name:'Name', score: 100},
+  {name:'Name', score: 20},
+  {name:'Name', score: 50},
+  {name:'Name', score: 78},
+  {name:'Name', score: 125},
+  {name:'Name', score: 77},
+  {name:'Name', score: 42},
+];
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'webpack';
-
-  return element;
-}
-
-document.body.appendChild(component());
+const listContent = document.querySelector('.list-content')
+data.forEach((item) => {
+  const {name, score} = item;
+  const li= document.createElement('li')
+  li.classList.add('list-item')
+  li.innerHTML=`${name}: ${score}`;
+  listContent.appendChild(li)
+});
