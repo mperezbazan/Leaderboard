aa9cf4370cee40b5171867d0b08926bd4fbd46e5
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-15 10:57:03 -0300

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import './style.css'
+import './style.css';
+
 const data = [
-  {name:'Name', score: 100},
-  {name:'Name', score: 20},
-  {name:'Name', score: 50},
-  {name:'Name', score: 78},
-  {name:'Name', score: 125},
-  {name:'Name', score: 77},
-  {name:'Name', score: 42},
+  { name: 'Name', score: 100 },
+  { name: 'Name', score: 20 },
+  { name: 'Name', score: 50 },
+  { name: 'Name', score: 78 },
+  { name: 'Name', score: 125 },
+  { name: 'Name', score: 77 },
+  { name: 'Name', score: 42 },
 ];
 
-const listContent = document.querySelector('.list-content')
+const listContent = document.querySelector('.list-content');
 data.forEach((item) => {
-  const {name, score} = item;
-  const li= document.createElement('li')
-  li.classList.add('list-item')
-  li.innerHTML=`${name}: ${score}`;
-  listContent.appendChild(li)
+  const { name, score } = item;
+  const li = document.createElement('li');
+  li.classList.add('list-item');
+  li.innerHTML = `${name}: ${score}`;
+  listContent.appendChild(li);
 });
