4da4ed425c5ae05a99c3c4e32ce10ae2b9529d4c
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-11-16 23:24:37 -0300

    Add request method to api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import './style.css';
 import Scores from './modules/Scores.js';
 
-const data = [
-  { name: 'Name', score: 100 },
-  { name: 'Name', score: 20 },
-  { name: 'Name', score: 50 },
-  { name: 'Name', score: 78 },
-  { name: 'Name', score: 125 },
-  { name: 'Name', score: 77 },
-  { name: 'Name', score: 42 },
-];
+const message = document.querySelector('.message');
+const getScores = async () => {
+  const data = await Scores.getData();
+  return data.result;
+};
+const sendScore = async (data) => {
+  const result = await Scores.postData(data);
+  return result.result;
+};
+const refreshButton = document.querySelector('#refresh');
+refreshButton.addEventListener('click', () => {
+  getScores().then((result) => Scores.render(result));
+});
 
-Scores(data);
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const { name, score } = e.target;
+  const newData = {
+    user: name.value,
+    score: parseInt(score.value, 10),
+  };
+  sendScore(newData).then((result) => {
+    message.classList.remove('display-none');
+    message.innerHTML = `${result} <br>Click on Refresh to update the list`;
+    name.value = '';
+    score.value = '';
+    getScores().then((result) => Scores.render(result));
+    setTimeout(() => {
+      message.classList.add('display-none');
+    }, 5000);
+  });
+});
+window.addEventListener('DOMContentLoaded', () => {
+  getScores().then((result) => Scores.render(result));
+});
